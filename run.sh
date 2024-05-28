46bc7a52f8a92cab3e88cf1386f72af9d74875d4
wellya <165001050+wellyamoela@users.noreply.github.com>
2024-05-28 15:45:29 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-82980056-a650-4256-96d1-ed364129b67d/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-2b512c2f-421e-4a85-b1c1-c921c038573a/)
 
 
 echo "$curl_output" >> Update.tsx
